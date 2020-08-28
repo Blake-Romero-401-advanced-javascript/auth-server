a2bd277f4d4225e93602e7a78f1663d37088801f
blakerom <blakeromero001@gmail.com>
2020-08-27 22:47:30 -0700

    working auth

--- src/server.js
+++ src/server.js
@@ -5,16 +5,24 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
+require('dotenv').config();
 
 app.use(cors());
 app.use(morgan('dev'));
 
+const errorHandler = require('./middleware/error.js');
+const notFound = require('./middleware/404.js');
 const authRouter = require('./auth/router.js');
+
+app.use(express.static('./public'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = {
   server: app,
