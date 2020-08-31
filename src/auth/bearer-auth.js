acd59198ae2bd5dffe117fd7a5de1079d62b6fb7
blakerom <blakeromero001@gmail.com>
2020-08-30 23:27:37 -0700

    working on fixes for lab 14

--- src/auth/bearer-auth.js
+++ src/auth/bearer-auth.js
@@ -1,19 +1,55 @@
 'use strict';
 
-const users = require('./users-model.js');
 
-module.exports = (req, res, next) => {
-  if (!req.headers.authorization) {
-    next('Invalid Login');
-    return;
-  }
+const User = require('./users-model');
 
+module.exports = async (req, res, next) => {
+
+  // req.headers.authorization should be : "Bearer slfldsf.alfjdslfjdsflj.sflasdjfdlsj"
+
+  if (!req.headers.authorization) { next('Invalid Login: Missing Headers'); return; }
+
+
+  // Pull out just the encoded part by splitting the header into an array on the space and popping off the 2nd element
   let token = req.headers.authorization.split(' ').pop();
 
-  users.authenticateToken(token)
-    .then(validUser => {
-      req.user = validUser;
-      next();
-    })
-    .catch(err => next('Invalid Login'));
-}
+  // Notice that here, we're catching the errors from the user model.
+  try {
+
+    const validUser = await User.authenticateToken(token);
+
+    req.user = validUser;
+
+    /* Lab 14 - add capabilities key/value pair */
+    req.user = {
+      username: validUser.username,
+      fullname: validUser.fullname,
+      email: validUser.email,
+      capabilities: validUser.capabilities,
+    };
+
+    next();
+
+  } catch (err) {
+
+    next('Invalid Login');
+  }
+};
+
+// const users = require('./users-model.js');
+
+// module.exports = (req, res, next) => {
+//   if (!req.headers.authorization) {
+//     next('Invalid Login');
+//     return;
+//   }
+
+//   let token = req.headers.authorization.split(' ').pop();
+
+//   users.authenticateToken(token)
+//     .then(validUser => {
+//       req.user = validUser;
+//       next();
+//     })
+//     .catch(err => next('Invalid Login'));
+// }
--- src/server.js
+++ src/server.js
@@ -1,26 +1,33 @@
 'use strict';
 
+//3rd Party Resources
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const app = express();
-require('dotenv').config();
 
-app.use(cors());
-app.use(morgan('dev'));
-
+//Esoteric Resources
 const errorHandler = require('./middleware/error.js');
 const notFound = require('./middleware/404.js');
 const authRouter = require('./auth/router.js');
+const extraRouter = require('./extra-routes.js');
+
+const app = express();
+// require('dotenv').config();
+
+//App level middleware
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use(express.static('./public'));
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//Routes
 app.use(authRouter);
+app.use(extraRouter);
 
+//Catchalls
 app.use(notFound);
 app.use(errorHandler);
 
